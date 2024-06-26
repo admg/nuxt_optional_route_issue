26493513064e148477f6794e1b46e921a7aa7be4
Adam Gluch <adam.gluch@gmail.com>
2024-06-26 14:44:43 +0200

    init

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <NuxtLink to="/a">Link to A</NuxtLink> <br />
+        <NuxtLink to="/a/id">Link to A + param</NuxtLink>
+    </div>
+
+    <div>
+        <NuxtLink to="/b">Link to B</NuxtLink> <br />
+        <NuxtLink to="/b/id">Link to B + param</NuxtLink>
+    </div>
+
+    <div>
+        <NuxtPage />
+    </div>
+</template>
+
+<style lang="css">
+.router-link-active {
+    background-color: yellow;
+}
+</style>
